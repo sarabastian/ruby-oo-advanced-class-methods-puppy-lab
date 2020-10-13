d15646fca2b670298bbe78f9fd7e6176bcef659a
sarabastian <seb118@georgetown.edu>
2020-10-13 17:11:13 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,38 @@
-# Add your code here
+require 'pry'
+
+class Dog
+
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def save
+    @@all << self
+    end
+    
+    def self.print_all
+        Dog.all.map do |dog|
+        puts dog.name
+        end
+    end
+
+  
+
+
+end
+
+hank = Dog.new("hank")
+joey = Dog.new("joey")
